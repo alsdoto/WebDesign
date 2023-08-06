7d71ac6f1681a7c1924440c93b17c8b09c715e34
alsdoto <alsdoto@gmail.com>
2023-08-06 11:42:49 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>This is About Page</h1>
+      <!-- Konten About Page -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutPage',
+    // ...
+  }
+  </script>
+  
